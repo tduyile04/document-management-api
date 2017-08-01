e0c06a8cbd9522323a306d1aaee56dccadc40707
tolupatrick004 <princericky2000@yahoo.com>
2017-08-01 18:37:34 +0100

    temp

---
+++ build/server/migrations/20170727173346-create-user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: function up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      roleId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Roles',
+          key: 'id',
+          as: 'roleId'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users');
+  }
+};
